2eb8fc7b084807f65766b1b58a15395bb2e3d1db
Lili Boxer <ninaliboxer@gmail.com>
2019-06-28 10:12:41 -0700

    some more testing experimenting setup

--- tests/makeResponse.test.js
+++ tests/makeResponse.test.js
@@ -1,7 +1,13 @@
+const { request } = require('supertest');
 const { makeResponse } = require('../lib/makeResponse');
+const { app } = require('../lib/app');
 
 describe('makes response', () => {
   it('path / with GET returns hi', () => {
-
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
   });
 });
