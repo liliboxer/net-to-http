29bdfa9b33b2b77a1c6c8e09e7e0d672986c1728
Lili Boxer <ninaliboxer@gmail.com>
2019-06-28 15:02:17 -0700

    colors tests working

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,13 @@
 const request = require('supertest');
 const { app } = require('../lib/app');
+const { red, green, blue } = require('../utils/color-utils');
 
 describe('parse request and send from server', () => {
   it('able to parse request', () => {
     return request(app)
       .get('/')
       .then(res => {
+        expect(res.status).toEqual(200);
         expect(res.text).toEqual(expect.stringContaining('hi'));
       });
   });
@@ -16,6 +18,33 @@ describe('parse request and send from server', () => {
       .then(res => {
         expect(res.status).toEqual(400);
         expect(res.text).toEqual(expect.stringContaining('sorry'));
+      });
+  });
+
+  it('/red returns red', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual(red);
+      });
+  });
+
+  it('/green returns green', () => {
+    return request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual(green);
+      });
+  });
+
+  it('/blue returns blue', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual(blue);
       });
   });
 });
