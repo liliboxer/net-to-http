be07709798adb944bb2a26a5fb271fa754bc2545
Lili Boxer <ninaliboxer@gmail.com>
2019-06-28 12:55:27 -0700

    testing for makeResponse working

--- tests/makeResponse.test.js
+++ tests/makeResponse.test.js
@@ -4,13 +4,13 @@ describe('makes response', () => {
   it('returns response as string', () => {
     const response = makeResponse('200 OK', 'hi', 'plain');
     expect(response).toEqual(`HTTP/1.1 200 OK
-      Date: ${new Date().toISOString()}
-      Server: Apache
-      Accept-Ranges: bytes
-      Content-Length: Buffer.from4
-      Content-Type: text/plain
-    
-      hi
+Date: ${new Date().toISOString()}
+Server: Apache
+Accept-Ranges: bytes
+Content-Length: Buffer.from2
+Content-Type: text/plain
+
+hi
   `);
 
   // given status and content, getitng the type yet 
