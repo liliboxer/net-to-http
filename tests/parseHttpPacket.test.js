d36def37793a8c7fe21694b4e73b2a4b1acc77df
Lili Boxer <ninaliboxer@gmail.com>
2019-06-28 08:55:59 -0700

    moved files around

--- tests/parseHttpPacket.test.js
+++ tests/parseHttpPacket.test.js
@@ -1,7 +1,7 @@
-const { parseHttpPacket } = require('./parseHttpPacket');
+const { parseHttpPacket } = require('../lib/parseHttpPacket');
 
 describe('http', () => {
-  it('returns method', () => {
+  it('returns method and path', () => {
     const str = 'GET / HTTP/1.1';
     const { method, path } = parseHttpPacket(str);
     expect(method).toEqual('GET');
